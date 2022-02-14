8377e3e446f0fb557152dc866d909f1fd627de8a
fperdigon <fperdigon88@gmail.com>
2022-02-14 14:25:16 -0500

    Statistical Diagrams Form renaming

---
+++ EMAnalizer 2.0/StatisticalDiagrams.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace EMAnalizer_2._0
+{
+    partial class Diagramas_estadisticos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series9 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series10 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Diagramas_estadisticos));
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart4 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.AxisX.Interval = 10D;
+            chartArea1.AxisX.IntervalOffset = 3D;
+            chartArea1.AxisX.IsLabelAutoFit = false;
+            chartArea1.AxisX.MajorGrid.Enabled = false;
+            chartArea1.AxisX.Maximum = 63D;
+            chartArea1.AxisX.Minimum = -63D;
+            chartArea1.AxisX.ScrollBar.Enabled = false;
+            chartArea1.AxisX.Title = "Desplazamiento del objetivo (°)";
+            chartArea1.AxisY.Interval = 10D;
+            chartArea1.AxisY.MajorGrid.Enabled = false;
+            chartArea1.AxisY.Maximum = 80D;
+            chartArea1.AxisY.Minimum = 0D;
+            chartArea1.AxisY.Title = "Desplazamiento del ojo (°)";
+            chartArea1.AxisY.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            chartArea1.AxisY2.Interval = 10D;
+            chartArea1.AxisY2.MajorGrid.Enabled = false;
+            chartArea1.AxisY2.Maximum = 80D;
+            chartArea1.AxisY2.Minimum = 0D;
+            chartArea1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Enabled = false;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Area;
+            series1.Color = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            series1.Legend = "Legend1";
+            series1.Name = "Series2";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Area;
+            series2.Color = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            series2.Legend = "Legend1";
+            series2.Name = "Series3";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series3.Legend = "Legend1";
+            series3.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            series3.Name = "Series1";
+            series3.YAxisType = System.Windows.Forms.DataVisualization.Charting.AxisType.Secondary;
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(384, 245);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            chartArea2.AxisX.Interval = 10D;
+            chartArea2.AxisX.IntervalOffset = 3D;
+            chartArea2.AxisX.IsLabelAutoFit = false;
+            chartArea2.AxisX.MajorGrid.Enabled = false;
+            chartArea2.AxisX.Maximum = 63D;
+            chartArea2.AxisX.Minimum = -63D;
+            chartArea2.AxisX.Title = "Desplazamiento del objetivo (°)";
+            chartArea2.AxisY.Interval = 100D;
+            chartArea2.AxisY.MajorGrid.Enabled = false;
+            chartArea2.AxisY.Maximum = 800D;
+            chartArea2.AxisY.Minimum = 0D;
+            chartArea2.AxisY.Title = "Velocidad sacádica (°/Seg)";
+            chartArea2.AxisY2.Interval = 100D;
+            chartArea2.AxisY2.MajorGrid.Enabled = false;
+            chartArea2.AxisY2.Maximum = 800D;
+            chartArea2.AxisY2.Minimum = 0D;
+            chartArea2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Enabled = false;
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(12, 285);
+            this.chart2.Name = "chart2";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Area;
+            series4.Color = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            series4.Legend = "Legend1";
+            series4.Name = "Series2";
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series5.Legend = "Legend1";
+            series5.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            series5.Name = "Series1";
+            series5.YAxisType = System.Windows.Forms.DataVisualization.Charting.AxisType.Secondary;
+            this.chart2.Series.Add(series4);
+            this.chart2.Series.Add(series5);
+            this.chart2.Size = new System.Drawing.Size(384, 245);
+            this.chart2.TabIndex = 1;
+            this.chart2.Text = "chart2";
+            // 
+            // chart3
+            // 
+            chartArea3.AxisX.Interval = 10D;
+            chartArea3.AxisX.IntervalOffset = 3D;
+            chartArea3.AxisX.IsLabelAutoFit = false;
+            chartArea3.AxisX.MajorGrid.Enabled = false;
+            chartArea3.AxisX.Maximum = 63D;
+            chartArea3.AxisX.Minimum = -63D;
+            chartArea3.AxisX.Title = "Desplazamiento del objetivo (°)";
+            chartArea3.AxisY.Interval = 2D;
+            chartArea3.AxisY.MajorGrid.Enabled = false;
+            chartArea3.AxisY.Maximum = 10D;
+            chartArea3.AxisY.Minimum = -10D;
+            chartArea3.AxisY.Title = "Desviación del ojo (°)";
+            chartArea3.AxisY2.Interval = 2D;
+            chartArea3.AxisY2.MajorGrid.Enabled = false;
+            chartArea3.AxisY2.Maximum = 10D;
+            chartArea3.AxisY2.Minimum = -10D;
+            chartArea3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            chartArea3.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea3);
+            legend3.Enabled = false;
+            legend3.Name = "Legend1";
+            this.chart3.Legends.Add(legend3);
+            this.chart3.Location = new System.Drawing.Point(422, 12);
+            this.chart3.Name = "chart3";
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Area;
+            series6.Color = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            series6.Legend = "Legend1";
+            series6.Name = "Series2";
+            series7.ChartArea = "ChartArea1";
+            series7.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Area;
+            series7.Color = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            series7.Legend = "Legend1";
+            series7.Name = "Series3";
+            series8.ChartArea = "ChartArea1";
+            series8.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series8.Legend = "Legend1";
+            series8.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            series8.Name = "Series1";
+            series8.YAxisType = System.Windows.Forms.DataVisualization.Charting.AxisType.Secondary;
+            this.chart3.Series.Add(series6);
+            this.chart3.Series.Add(series7);
+            this.chart3.Series.Add(series8);
+            this.chart3.Size = new System.Drawing.Size(394, 245);
+            this.chart3.TabIndex = 2;
+            this.chart3.Text = "chart3";
+            // 
+            // chart4
+            // 
+            this.chart4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea4.AxisX.Interval = 10D;
+            chartArea4.AxisX.IntervalOffset = 3D;
+            chartArea4.AxisX.IsLabelAutoFit = false;
+            chartArea4.AxisX.MajorGrid.Enabled = false;
+            chartArea4.AxisX.Maximum = 63D;
+            chartArea4.AxisX.Minimum = -63D;
+            chartArea4.AxisX.Title = "Desplazamiento del objetivo (°)";
+            chartArea4.AxisY.Interval = 100D;
+            chartArea4.AxisY.MajorGrid.Enabled = false;
+            chartArea4.AxisY.Maximum = 500D;
+            chartArea4.AxisY.Minimum = 0D;
+            chartArea4.AxisY.Title = "Latencia de la sácada (ms)";
+            chartArea4.AxisY2.Interval = 100D;
+            chartArea4.AxisY2.MajorGrid.Enabled = false;
+            chartArea4.AxisY2.Maximum = 500D;
+            chartArea4.AxisY2.Minimum = 0D;
+            chartArea4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            chartArea4.Name = "ChartArea1";
+            this.chart4.ChartAreas.Add(chartArea4);
+            legend4.Enabled = false;
+            legend4.Name = "Legend1";
+            this.chart4.Legends.Add(legend4);
+            this.chart4.Location = new System.Drawing.Point(422, 285);
+            this.chart4.Name = "chart4";
+            series9.ChartArea = "ChartArea1";
+            series9.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Area;
+            series9.Color = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            series9.Legend = "Legend1";
+            series9.Name = "Series2";
+            series10.ChartArea = "ChartArea1";
+            series10.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series10.Legend = "Legend1";
+            series10.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            series10.Name = "Series1";
+            series10.YAxisType = System.Windows.Forms.DataVisualization.Charting.AxisType.Secondary;
+            this.chart4.Series.Add(series9);
+            this.chart4.Series.Add(series10);
+            this.chart4.Size = new System.Drawing.Size(394, 245);
+            this.chart4.TabIndex = 3;
+            this.chart4.Text = "chart4";
+            // 
+            // Diagramas_estadisticos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(828, 544);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart4);
+            this.Controls.Add(this.chart3);
+            this.Controls.Add(this.chart1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Diagramas_estadisticos";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Diagramas estadisticos";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart4;
+    }
+}
---
+++ EMAnalizer 2.0/StatisticalDiagramsForm.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EMAnalizer_2._0
+{
+    public partial class Diagramas_estadisticos : Form
+    {
+        Pruebas P;
+        public Diagramas_estadisticos()
+        {
+            InitializeComponent();
+        }
+        public Diagramas_estadisticos(Pruebas p)
+        {
+            InitializeComponent();
+            P = p;
+            
+            //Dibujo de lineas
+            //primer diagrama
+            chart1.Series[1].Points.DataBindXY(new int[3] { -63, 0, 63 }, new double[3] { 50.4, 0, 50.4 });
+            chart1.Series[0].Points.DataBindXY(new int[3] { -63, 0, 63 }, new double[3] { 75.6,0, 75.6 });
+            
+            //segundo diagrama
+            //chart2.Series[1].Points.DataBindXY(new int[13] { -68,-54, -40,-30, -20, -12, 0, 12, 20, 30, 40, 54, 68 }, new double[13] { 480, 430, 375, 320, 240, 150, 0, 150, 240, 320, 375, 430, 480 });
+            chart2.Series[0].Points.DataBindXY(new int[13] { -68, -54, -40, -30, -20, -12, 0, 12, 20, 30, 40, 54, 68 }, new double[13] { 380, 330, 275, 220, 140, 50, -100, 50, 140, 220, 275, 330, 380 });
+            //tercer diagrama
+            chart3.Series[0].Points.DataBindXY(new int[3] { -63, 0, 63 }, new double[3] { 8.4, 0, 8.4 });
+            chart3.Series[1].Points.DataBindXY(new int[3] { -63, 0, 63 }, new double[3] { -8.4, 0, -8.4 });
+            
+            //cuarto diagrama
+            chart4.Series[0].Points.DataBindXY(new int[3] { -63, 0, 63 }, new double[3] {275, 200, 275 });
+            
+
+
+            float AmEst;
+            for (int i = 1; i < P.CantPruebas -1; i++) { 
+                for (int j = 1; j < P.ASacadas[i].Length-1; j++) {
+
+                    
+                    AmEst= P.SEstimulo[i][(P.SacadasI[i][j])] - P.SEstimulo[i][(P.SacadasI[i][j-1])];
+                    if (AmEst == 0 && j>1) {
+                        AmEst = P.SEstimulo[i][(P.SacadasI[i][j])] - P.SEstimulo[i][(P.SacadasI[i][j - 2])];
+                    }
+
+                    if (AmEst == 0 && j <= 1)
+                    {
+                        AmEst = P.SEstimulo[i][(P.SacadasI[i][j])]*2;
+                    }
+
+                    //Amplitud de las sacadas
+                    chart1.Series[2].Points.InsertXY(0, AmEst, Math.Abs(P.ASacadas[i][j]));          
+                    //Velocidad sacadica
+                    chart2.Series[1].Points.InsertXY(0, AmEst, Math.Abs((P.ASacadas[i][j] * P.Fs) / (P.SacadasF[i][j] - P.SacadasI[i][j]))); 
+                    //Desviacion
+                    chart3.Series[2].Points.InsertXY(0, AmEst, P.ASacadas[i][j] - AmEst);
+                    //Latencia
+                    chart4.Series[1].Points.InsertXY(0, AmEst, P.Latencia[i][j]); 
+                }
+            }
+
+            
+        }
+               
+
+        
+    }
+}
