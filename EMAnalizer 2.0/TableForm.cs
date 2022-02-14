07cfe9d8e2bd93092c835e8d9630618087a0e0eb
fperdigon <fperdigon88@gmail.com>
2022-02-14 17:22:00 -0500

    Deleting unuseful code

--- EMAnalizer 2.0/TableForm.cs
+++ EMAnalizer 2.0/TableForm.cs
@@ -13,13 +13,13 @@ namespace EMAnalizer_2._0
     public partial class TableForm : Form
     {        
 
-        public TableForm(Test_T P)
+        public TableForm(Test_T test)
         {
             InitializeComponent();
 
 
             int i = 2;//poner numero corecto
-			dataGridView1.ColumnCount = P.ASacadas[i].Length - 1;            
+			dataGridView1.ColumnCount = test.ASacadas[i].Length - 1;            
 
             dataGridView1.ColumnHeadersDefaultCellStyle.BackColor = Color.Navy;
             dataGridView1.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
@@ -35,29 +35,29 @@ namespace EMAnalizer_2._0
             dataGridView1.Columns[0].Name = "Parametros";
             int AmEst = 10;
             
-            for (int j = 1; j < P.ASacadas[i].Length-1 ; j++)
+            for (int j = 1; j < test.ASacadas[i].Length-1 ; j++)
             {
                 dataGridView1.Columns[j].Name = j.ToString();
             }
-            string[] row1 = new string[P.ASacadas[i].Length];
-            row1[0] = "Amplitud (°)";
-            string[] row2 = new string[P.ASacadas[i].Length];
-            row2[0] = "Desviación (°)";
-            string[] row3 = new string[P.ASacadas[i].Length];
-            row3[0] = "Velocidad (°/seg)";
-            string[] row4 = new string[P.ASacadas[i].Length];
-            row4[0] = "Latencia (ms)";
+            string[] row1 = new string[test.ASacadas[i].Length];
+            string[] row2 = new string[test.ASacadas[i].Length];
+            string[] row3 = new string[test.ASacadas[i].Length];
+            string[] row4 = new string[test.ASacadas[i].Length];            
             
-            for (int j = 1; j < P.ASacadas[i].Length-1 ; j++)
+            row1[0] = "Amplitude (°)";
+            row2[0] = "Deviation (°)";
+            row3[0] = "Velocity (°/seg)";
+            row4[0] = "Latency (ms)";
+            
+            for (int j = 1; j < test.ASacadas[i].Length-1 ; j++)
             {
-                row1[j] = Math.Abs(P.ASacadas[i][j]).ToString();
-                row2[j] = (Math.Abs(P.ASacadas[i][j]) - AmEst).ToString();
-                row3[j] = Math.Abs((P.ASacadas[i][j] * P.Fs) / (P.SacadasF[i][j] - P.SacadasI[i][j])).ToString();
-                row4[j] = P.Latencia[i][j].ToString();
+                row1[j] = Math.Abs(test.ASacadas[i][j]).ToString();
+                row2[j] = (Math.Abs(test.ASacadas[i][j]) - AmEst).ToString();
+                row3[j] = Math.Abs((test.ASacadas[i][j] * test.Fs) / (test.SacadasF[i][j] - test.SacadasI[i][j])).ToString();
+                row4[j] = test.Latencia[i][j].ToString();
             }           
            
-            //para dar estilo
-            dataGridView1.Columns[4].DefaultCellStyle.Font = new Font(dataGridView1.DefaultCellStyle.Font, FontStyle.Italic);           
+            //para dar estilo                       
             dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
             dataGridView1.MultiSelect = false;
             dataGridView1.Dock = DockStyle.Fill;           
@@ -73,7 +73,7 @@ namespace EMAnalizer_2._0
 
             i = 2;//poner numero corecto  
             //numero de columnas
-            dataGridView2.ColumnCount = P.ASacadas[i].Length - 1;
+            dataGridView2.ColumnCount = test.ASacadas[i].Length - 1;
 
             dataGridView2.ColumnHeadersDefaultCellStyle.BackColor = Color.Navy;
             dataGridView2.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
@@ -89,29 +89,25 @@ namespace EMAnalizer_2._0
             dataGridView2.Columns[0].Name = "Parametros";
             AmEst = 20;
 
-            for (int j = 1; j < P.ASacadas[i].Length - 1; j++)
+            for (int j = 1; j < test.ASacadas[i].Length - 1; j++)
             {
                 dataGridView2.Columns[j].Name = j.ToString();
             }
             
-            row1[0] = "Amplitud (°)";
-            
-            row2[0] = "Desviación (°)";
-            
-            row3[0] = "Velocidad (°/seg)";
-            
-            row4[0] = "Latencia (ms)";
+            row1[0] = "Amplitude (°)";
+            row2[0] = "Deviation (°)";
+            row3[0] = "Velocity (°/seg)";
+            row4[0] = "Latency (ms)";
 
-            for (int j = 1; j < P.ASacadas[i].Length - 1; j++)
+            for (int j = 1; j < test.ASacadas[i].Length - 1; j++)
             {
-                row1[j] = Math.Abs(P.ASacadas[i][j]).ToString();
-                row2[j] = (Math.Abs(P.ASacadas[i][j]) - AmEst).ToString();
-                row3[j] = Math.Abs((P.ASacadas[i][j] * P.Fs) / (P.SacadasF[i][j] - P.SacadasI[i][j])).ToString();
-                row4[j] = P.Latencia[i][j].ToString();
+                row1[j] = Math.Abs(test.ASacadas[i][j]).ToString();
+                row2[j] = (Math.Abs(test.ASacadas[i][j]) - AmEst).ToString();
+                row3[j] = Math.Abs((test.ASacadas[i][j] * test.Fs) / (test.SacadasF[i][j] - test.SacadasI[i][j])).ToString();
+                row4[j] = test.Latencia[i][j].ToString();
             }
 
-            //para dar estilo
-            dataGridView2.Columns[4].DefaultCellStyle.Font = new Font(dataGridView1.DefaultCellStyle.Font, FontStyle.Italic);
+            //para dar estilo            
             dataGridView2.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
             dataGridView2.MultiSelect = false;
             dataGridView2.Dock = DockStyle.Fill;
@@ -126,7 +122,7 @@ namespace EMAnalizer_2._0
 
             i = 3;//poner numero corecto  
             //numero de columnas
-            dataGridView3.ColumnCount = P.ASacadas[i].Length - 1;
+            dataGridView3.ColumnCount = test.ASacadas[i].Length - 1;
 
             dataGridView3.ColumnHeadersDefaultCellStyle.BackColor = Color.Navy;
             dataGridView3.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
@@ -142,29 +138,25 @@ namespace EMAnalizer_2._0
             dataGridView3.Columns[0].Name = "Parametros";
             AmEst = 30;
 
-            for (int j = 1; j < P.ASacadas[i].Length - 1; j++)
+            for (int j = 1; j < test.ASacadas[i].Length - 1; j++)
             {
                 dataGridView3.Columns[j].Name = j.ToString();
             }
 
-            row1[0] = "Amplitud (°)";
+            row1[0] = "Amplitude (°)";
+            row2[0] = "Deviation (°)";
+            row3[0] = "Velocity (°/seg)";
+            row4[0] = "Latency (ms)";
 
-            row2[0] = "Desviación (°)";
-
-            row3[0] = "Velocidad (°/seg)";
-
-            row4[0] = "Latencia (ms)";
-
-            for (int j = 1; j < P.ASacadas[i].Length - 1; j++)
+            for (int j = 1; j < test.ASacadas[i].Length - 1; j++)
             {
-                row1[j] = Math.Abs(P.ASacadas[i][j]).ToString();
-                row2[j] = (Math.Abs(P.ASacadas[i][j]) - AmEst).ToString();
-                row3[j] = Math.Abs((P.ASacadas[i][j] * P.Fs) / (P.SacadasF[i][j] - P.SacadasI[i][j])).ToString();
-                row4[j] = P.Latencia[i][j].ToString();
+                row1[j] = Math.Abs(test.ASacadas[i][j]).ToString();
+                row2[j] = (Math.Abs(test.ASacadas[i][j]) - AmEst).ToString();
+                row3[j] = Math.Abs((test.ASacadas[i][j] * test.Fs) / (test.SacadasF[i][j] - test.SacadasI[i][j])).ToString();
+                row4[j] = test.Latencia[i][j].ToString();
             }
 
-            //para dar estilo
-            dataGridView3.Columns[4].DefaultCellStyle.Font = new Font(dataGridView1.DefaultCellStyle.Font, FontStyle.Italic);
+            //para dar estilo            
             dataGridView3.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
             dataGridView3.MultiSelect = false;
             dataGridView3.Dock = DockStyle.Fill;
@@ -179,7 +171,7 @@ namespace EMAnalizer_2._0
 
             i = 4;//poner numero corecto  
             //numero de columnas
-            dataGridView4.ColumnCount = P.ASacadas[i].Length - 1;
+            dataGridView4.ColumnCount = test.ASacadas[i].Length - 1;
 
             dataGridView4.ColumnHeadersDefaultCellStyle.BackColor = Color.Navy;
             dataGridView4.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
@@ -195,29 +187,25 @@ namespace EMAnalizer_2._0
             dataGridView4.Columns[0].Name = "Parametros";
             AmEst = 60;
 
-            for (int j = 1; j < P.ASacadas[i].Length - 1; j++)
+            for (int j = 1; j < test.ASacadas[i].Length - 1; j++)
             {
                 dataGridView4.Columns[j].Name = j.ToString();
             }
 
-            row1[0] = "Amplitud (°)";
+            row1[0] = "Amplitude (°)";
+            row2[0] = "Deviation (°)";
+            row3[0] = "Velocity (°/seg)";
+            row4[0] = "Latency (ms)";
 
-            row2[0] = "Desviación (°)";
-
-            row3[0] = "Velocidad (°/seg)";
-
-            row4[0] = "Latencia (ms)";
-
-            for (int j = 1; j < P.ASacadas[i].Length - 1; j++)
+            for (int j = 1; j < test.ASacadas[i].Length - 1; j++)
             {
-                row1[j] = Math.Abs(P.ASacadas[i][j]).ToString();
-                row2[j] = (Math.Abs(P.ASacadas[i][j]) - AmEst).ToString();
-                row3[j] = Math.Abs((P.ASacadas[i][j] * P.Fs) / (P.SacadasF[i][j] - P.SacadasI[i][j])).ToString();
-                row4[j] = P.Latencia[i][j].ToString();
+                row1[j] = Math.Abs(test.ASacadas[i][j]).ToString();
+                row2[j] = (Math.Abs(test.ASacadas[i][j]) - AmEst).ToString();
+                row3[j] = Math.Abs((test.ASacadas[i][j] * test.Fs) / (test.SacadasF[i][j] - test.SacadasI[i][j])).ToString();
+                row4[j] = test.Latencia[i][j].ToString();
             }
 
-            //para dar estilo
-            dataGridView4.Columns[4].DefaultCellStyle.Font = new Font(dataGridView1.DefaultCellStyle.Font, FontStyle.Italic);
+            //para dar estilo            
             dataGridView4.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
             dataGridView4.MultiSelect = false;
             dataGridView4.Dock = DockStyle.Fill;
