c8db27c2c882d057cb26e064e04d7f5a3607fa46
fperdigon <fperdigon88@gmail.com>
2022-02-15 10:35:31 -0500

    Translating Statistical Diagrams Form. Adding axes names programatically

--- EMAnalizer 2.0/StatisticalDiagramsForm.cs
+++ EMAnalizer 2.0/StatisticalDiagramsForm.cs
@@ -22,22 +22,28 @@ namespace EMAnalizer_2._0
             InitializeComponent();
             P = p;
             
-            //Dibujo de lineas
-            //primer diagrama
+            // Lines drawing and axis naming
+            // Diagram 1
             chart1.Series[1].Points.DataBindXY(new int[3] { -63, 0, 63 }, new double[3] { 50.4, 0, 50.4 });
             chart1.Series[0].Points.DataBindXY(new int[3] { -63, 0, 63 }, new double[3] { 75.6,0, 75.6 });
+            chart1.ChartAreas[0].Axes[0].Title = "Target shift (°)";
+            chart1.ChartAreas[0].Axes[1].Title = "Eye shift (°)";
             
-            //segundo diagrama
-            //chart2.Series[1].Points.DataBindXY(new int[13] { -68,-54, -40,-30, -20, -12, 0, 12, 20, 30, 40, 54, 68 }, new double[13] { 480, 430, 375, 320, 240, 150, 0, 150, 240, 320, 375, 430, 480 });
+            // Diagram 2            
             chart2.Series[0].Points.DataBindXY(new int[13] { -68, -54, -40, -30, -20, -12, 0, 12, 20, 30, 40, 54, 68 }, new double[13] { 380, 330, 275, 220, 140, 50, -100, 50, 140, 220, 275, 330, 380 });
-            //tercer diagrama
+            chart2.ChartAreas[0].Axes[0].Title = "Target shift (°)";
+            chart2.ChartAreas[0].Axes[1].Title = "Sacade velocity (°/s)";            
+            
+            // Diagram 3
             chart3.Series[0].Points.DataBindXY(new int[3] { -63, 0, 63 }, new double[3] { 8.4, 0, 8.4 });
             chart3.Series[1].Points.DataBindXY(new int[3] { -63, 0, 63 }, new double[3] { -8.4, 0, -8.4 });
+            chart3.ChartAreas[0].Axes[0].Title = "Target shift (°)";
+            chart3.ChartAreas[0].Axes[1].Title = "Eye shift error (°)";
             
-            //cuarto diagrama
+            // Diagram 4
             chart4.Series[0].Points.DataBindXY(new int[3] { -63, 0, 63 }, new double[3] {275, 200, 275 });
-            
-
+            chart4.ChartAreas[0].Axes[0].Title = "Target shift (°)";
+            chart4.ChartAreas[0].Axes[1].Title = "Sacade latency (ms)";
 
             float AmEst;
             for (int i = 1; i < P.CantPruebas -1; i++) { 
@@ -54,14 +60,17 @@ namespace EMAnalizer_2._0
                         AmEst = P.SEstimulo[i][(P.SacadasI[i][j])]*2;
                     }
 
-                    //Amplitud de las sacadas
+                    // Sacade amplitude
                     chart1.Series[2].Points.InsertXY(0, AmEst, Math.Abs(P.ASacadas[i][j]));          
-                    //Velocidad sacadica
+                    // Sacade Velocity
                     chart2.Series[1].Points.InsertXY(0, AmEst, Math.Abs((P.ASacadas[i][j] * P.Fs) / (P.SacadasF[i][j] - P.SacadasI[i][j]))); 
-                    //Desviacion
+                    // Eye shift error
                     chart3.Series[2].Points.InsertXY(0, AmEst, P.ASacadas[i][j] - AmEst);
-                    //Latencia
+                    // Latency
                     chart4.Series[1].Points.InsertXY(0, AmEst, P.Latencia[i][j]); 
+                    
+                    
+                    
                 }
             }
 
